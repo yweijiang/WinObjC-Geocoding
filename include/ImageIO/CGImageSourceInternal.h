f3da89c68f5f2c5b23902b17a669c69c2fbd3341
Varun Venkatesan <varun.k.venkatesan@intel.com>
2016-03-22 13:10:50 -0700

    First ImageIO contribution - Adding more files

---
+++ include/ImageIO/CGImageSourceInternal.h
@@ -0,0 +1,41 @@
+//******************************************************************************
+//
+// Copyright (c) 2016, Intel Corporation
+// Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+#pragma once
+
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CGDataProvider.h>
+#import <ImageIO/ImageIO.h>
+#import <ImageIO/CGImageSource.h>
+#import <objc/runtime.h>
+#import <Starboard.h>
+#include <NSLogging.h>
+#include <vector>
+
+#include "COMIncludes.h"
+#include "Wincodec.h"
+#include <wrl/client.h>
+#include "COMIncludes_End.h"
+
+using namespace Microsoft::WRL;
+
+@interface ImageSource : NSObject
+@property (nonatomic) NSData *data;
+- (instancetype)initWithData:(CFDataRef)data;
+- (instancetype)initWithURL:(CFURLRef)url;
+- (instancetype)initWithDataProvider:(CGDataProviderRef)provider;
+- (CFStringRef)getImageType;
+@end
