ffcb76c2bdb05a9ac711d8d74f99dbed31eed5e9
Shahmeer Esmail <shahmeer.esmail@intel.com>
2016-06-16 10:55:49 -0700

    Variables renamex, formatting changes.

--- tests/unittests/QuartzCore/QuartzCoreTest.mm
+++ tests/unittests/QuartzCore/QuartzCoreTest.mm
@@ -31,10 +31,13 @@ struct Dword32 {
 
 static const float c_ComparisonEpsilon = 0.0000025f;
 
-static void checkMatrixWithinTolerance(
-    const char* pStr, const float* pM, const float* pMGolden, int dimension = 4, float tolerance = c_ComparisonEpsilon) {
-    const unsigned int* pMUInt = reinterpret_cast<const unsigned int*>(pM);
-    const unsigned int* pMGoldenUInt = reinterpret_cast<const unsigned int*>(pMGolden);
+static void checkMatrixWithinTolerance(const char* errorString,
+                                       const void* matrixTypeless,
+                                       const void* goldenMatrixTypeless,
+                                       int dimension = 4,
+                                       float tolerance = c_ComparisonEpsilon) {
+    const Dword32* matrix = reinterpret_cast<const Dword32*>(matrixTypeless);
+    const Dword32* goldenMatrix = reinterpret_cast<const Dword32*>(goldenMatrixTypeless);
 
     assert(dimension <= 4);
 
@@ -42,18 +45,19 @@ static void checkMatrixWithinTolerance(
 
     for (int i = 0; i < dimension; i++) {
         for (int j = 0; j < dimension; j++) {
-            // Catch cases where element data are identical but ASSERT_NEAR and ASSERT_EQ identifies as different
-            // Specifically, ASSERT_NEAR incorrectly marks +/- NaN values as not being identical
-            if (pMUInt[index] != pMGoldenUInt[index]) {
-                ASSERT_NEAR_MSG(pM[index],
-                                pMGolden[index],
+            // Catch cases where element data are for all relevant intents and purposes identical but 
+            // ASSERT_NEAR and ASSERT_EQ identifies as different.
+            // Specifically, ASSERT_NEAR incorrectly marks +/- NaN values as not being identical.
+            if (matrix[index].u32 != goldenMatrix[index].u32) {
+                ASSERT_NEAR_MSG(matrix[index].f32,
+                                goldenMatrix[index].f32,
                                 tolerance,
                                 "TEST FAILED: %s \n\tMatrix mismatch at M[%i][%i]\n\t\tGOLDEN: %f\n\t\tACTUAL: %f\n",
-                                pStr,
+                                errorString,
                                 i,
                                 j,
-                                pMGolden[index],
-                                pM[index]);
+                                goldenMatrix[index].f32,
+                                matrix[index].f32);
             }
 
             index++;
@@ -91,19 +95,20 @@ TEST(QuartzCore, MatrixMath) {
 
     // Generate rotation matrix
     CATransform3D rotMatrix = CATransform3DMakeRotation(rotAngle, rotAxis[0], rotAxis[1], rotAxis[2]);
-    checkMatrixWithinTolerance("FAILED: CATransform3DMakeRotation", rotMatrix.m[0], &rotGolden[0].f32);
+    checkMatrixWithinTolerance("FAILED: CATransform3DMakeRotation", rotMatrix.m[0], rotGolden);
 
     // Generate translation matrix
     CATransform3D transMatrix = CATransform3DMakeTranslation(trans[0], trans[1], trans[2]);
-    checkMatrixWithinTolerance("FAILED: CATransform3DMakeTranslation", transMatrix.m[0], &transGolden[0].f32);
+    checkMatrixWithinTolerance("FAILED: CATransform3DMakeTranslation", transMatrix.m[0], transGolden);
 
     // Rotation + Translation via Concat of above matrices
     CATransform3D rotTransMatrixConcat = CATransform3DConcat(rotMatrix, transMatrix);
-    checkMatrixWithinTolerance("FAILED: CATransform3DConcat", rotTransMatrixConcat.m[0], &rotTransGolden[0].f32);
+    checkMatrixWithinTolerance("FAILED: CATransform3DConcat", rotTransMatrixConcat.m[0], rotTransGolden);
 
     // Rotation + Translation via premultiply methods CATransform3DTranslate and CATransform3DRotate
     CATransform3D rotTransMatrixPreMul = CATransform3DTranslate(CATransform3DIdentity, trans[0], trans[1], trans[2]);
+
     // Pre-multiply rotation matrix to get matrix which rotates then translates
     rotTransMatrixPreMul = CATransform3DRotate(rotTransMatrixPreMul, rotAngle, rotAxis[0], rotAxis[1], rotAxis[2]);
-    checkMatrixWithinTolerance("FAILED: CATransform3DTranslate and CATransform3dRotate", rotTransMatrixPreMul.m[0], &rotTransGolden[0].f32);
+    checkMatrixWithinTolerance("FAILED: CATransform3DTranslate and CATransform3dRotate", rotTransMatrixPreMul.m[0], rotTransGolden);
 }
