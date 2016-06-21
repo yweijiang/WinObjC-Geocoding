4c6702bb3a0d57e061a71540032541b3c96a42d4
Shahmeer Esmail <shahmeer.esmail@intel.com>
2016-06-21 11:52:36 -0700

    Added vImage test case to WOCCATALOG for new vImage APIs.

--- samples/WOCCatalog/WOCCatalog/ImagesViewController.m
+++ samples/WOCCatalog/WOCCatalog/ImagesViewController.m
@@ -18,6 +18,7 @@
 #import <CoreImage/CIImage.h>
 #import <CoreImage/CIContext.h>
 #import <CoreGraphics/CGImage.h>
+#import <Accelerate/vImage.h>
 
 @implementation ImagesViewController
 
@@ -36,6 +37,84 @@
     UIGraphicsEndImageContext();
 
     return scaledImage;
+}
+
++ (UIImage*)filterImage:(CGImageRef)imageRef {
+    const CGColorSpaceRef colorSpace = CGImageGetColorSpace(imageRef);
+    const CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    const uint32_t version = 0;
+    const CGFloat decode = 0.0f;
+    const CGColorRenderingIntent renderingIntent = CGImageGetRenderingIntent(imageRef);
+    const uint32_t bitsPerComponent = (uint32_t)CGImageGetBitsPerComponent(imageRef);
+    const uint32_t bitsPerPixel =
+        bitsPerComponent * (uint32_t)(CGColorSpaceGetNumberOfComponents(colorSpace) + (kCGImageAlphaNone != CGImageGetAlphaInfo(imageRef)));
+
+    vImage_CGImageFormat imageFormatStruct = { bitsPerComponent, bitsPerPixel, colorSpace, bitmapInfo, version, &decode, renderingIntent };
+
+    vImage_Buffer imageBufferRGBA;
+    vImageBuffer_InitWithCGImage(&imageBufferRGBA, &imageFormatStruct, nil, imageRef, 0);
+
+    vImage_Buffer imageBufferR;
+    vImage_Buffer imageBufferG;
+    vImage_Buffer imageBufferB;
+    vImage_Buffer imageBufferA;
+
+    vImageBuffer_Init(&imageBufferR, imageBufferRGBA.height, imageBufferRGBA.width, bitsPerComponent, 0);
+    vImageBuffer_Init(&imageBufferG, imageBufferR.height, imageBufferR.width, bitsPerComponent, 0);
+    vImageBuffer_Init(&imageBufferB, imageBufferR.height, imageBufferR.width, bitsPerComponent, 0);
+    vImageBuffer_Init(&imageBufferA, imageBufferR.height, imageBufferR.width, bitsPerComponent, 0);
+
+	// Note: Although the function calls for an ARGB input, RGBA input can be used if the output planes are swizzled
+    vImageConvert_ARGB8888toPlanar8(&imageBufferRGBA, &imageBufferR, &imageBufferG, &imageBufferB, &imageBufferA, 0);
+
+    const uint32_t height = imageBufferRGBA.height;
+    const uint32_t endOfFirstSlice = height / 3;
+    const uint32_t endOfSecondSlice = endOfFirstSlice * 2;
+    const uint32_t endOfThirdSlice = height;
+
+    // Divide the image into three slices and remove components to produce strips with different colors
+    
+    // Slice 0: Remove Red to produce Cyan output
+    char* colorData;
+    uint32_t rowPitch;
+
+    rowPitch = imageBufferR.rowBytes;
+    colorData = (char*)(imageBufferR.data);
+    for (uint32_t i = 0; i < endOfFirstSlice; i++) {
+        memset(colorData, 0, rowPitch);
+        colorData += rowPitch;
+    }
+
+    // Slice 1: Remove Green to produce Magenta output
+    rowPitch = imageBufferG.rowBytes;
+    colorData = (char*)(imageBufferG.data) + endOfFirstSlice * rowPitch;
+    for (uint32_t i = endOfFirstSlice; i < endOfSecondSlice; i++) {
+        memset(colorData, 0, rowPitch);
+        colorData += rowPitch;
+    }
+
+    // Slice 2: Remove Blue to produce Yellow output
+    rowPitch = imageBufferB.rowBytes;
+    colorData = (char*)(imageBufferB.data) + endOfSecondSlice * rowPitch;
+    for (uint32_t i = endOfSecondSlice; i < endOfThirdSlice; i++) {
+        memset(colorData, 0, rowPitch);
+        colorData += rowPitch;
+    }
+
+    // Note: To get RGBA output, input planes need to be swizzled
+    vImageConvert_Planar8toARGB8888(&imageBufferR, &imageBufferG, &imageBufferB, &imageBufferA, &imageBufferRGBA, 0);
+
+    CGImageRef cgImageFromBuffer = vImageCreateCGImageFromBuffer(&imageBufferRGBA, &imageFormatStruct, nil, nil, 0, nil);
+    UIImage* uiImageFromBuffer = [UIImage imageWithCGImage:cgImageFromBuffer];
+    CGImageRelease(cgImageFromBuffer);
+
+    free(imageBufferRGBA.data);
+    free(imageBufferA.data);
+    free(imageBufferR.data);
+    free(imageBufferG.data);
+    free(imageBufferB.data);
+
+    return uiImageFromBuffer;
 }
 
 - (void)viewDidLoad {
@@ -58,18 +137,22 @@
     CIImage* ciImage = [CIImage imageWithCGImage:photo.CGImage];
     CGImageRef cgImage = [context createCGImage:ciImage fromRect:CGRectMake(300, 600, 200, 200)];
 
+
+    UIImage* filteredImage = [ImagesViewController filterImage : photo.CGImage];
+
     imagesView.animationImages = [NSArray arrayWithObjects:
-                            scaledPhotoHighInterpolation,
+                            /*scaledPhotoHighInterpolation,
                             scaledPhotoNoInterpolation,
                             [UIImage imageNamed:@"photo1.jpg"],
                             [UIImage imageNamed:@"photo2.jpg"],
-                            [UIImage imageWithCGImage:cgImage],
+                            [UIImage imageWithCGImage:cgImage],*/
+                            filteredImage,/*
                             [UIImage imageNamed:@"photo3.jpg"],
                             [UIImage imageNamed:@"photo4.jpg"],
                             [UIImage imageNamed:@"photo5.jpg"],
                             [UIImage imageNamed:@"photo6.jpg"],
                             [UIImage imageNamed:@"photo7.gif"],
-                            [UIImage imageNamed:@"photo8.tif"],
+                            [UIImage imageNamed:@"photo8.tif"],*/
                             nil];
 
     imagesView.animationDuration = 10.0;
