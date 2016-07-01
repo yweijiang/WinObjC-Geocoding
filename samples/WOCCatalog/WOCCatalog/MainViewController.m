2daf69d6a2bd21967f2fb5b188d2912747e3a4a0
Yusheng Weijiang <yusheng.weijiang@intel.com>
2016-06-30 17:06:47 -0700

    Added all changes from previous heading work

--- samples/WOCCatalog/WOCCatalog/MainViewController.m
+++ samples/WOCCatalog/WOCCatalog/MainViewController.m
@@ -41,8 +41,7 @@
 #import "AccelerateViewController.h"
 #import "CoreMotionViewController.h"
 #import "ShadowViewController.h"
-#import "UIPasteboardViewController.h"
-#import "AudioToolboxViewController.h"
+#import "CoreLocationViewController.h"
 
 #ifdef WINOBJC
 #import "XamlViewController.h"
@@ -59,12 +58,12 @@
     [super viewDidLoad];
     self.menuItems = [NSMutableArray array];
     
-    //AudioToolbox
-    [self addMenuItemViewController:[[AudioToolboxViewController alloc] init] andTitle:@"AudioToolbox"];
-   
-    //CoreMotion
-    [self addMenuItemViewController:[[CoreMotionViewController alloc] init] andTitle:@"CoreMotion"];
+    // CoreLocation
+    [self addMenuItemViewController:[[CoreLocationViewController alloc] init] andTitle:@"CoreLocation"];
 
+    // CoreMotion
+    [self addMenuItemViewController:[[CoreMotionViewController alloc] init] andTitle:@"CoreMotion"];
+    
     // Foundations tests
     [self addMenuItemViewController:[[FoundationsViewController alloc] init] andTitle:@"Foundation Tests"];
 
@@ -132,19 +131,16 @@
 
     // Basic Animation
     [self addMenuItemViewController:[[BasicAnimationViewController alloc] init] andTitle:@"Animation"];
-
+    
     // Accelerate
     [self addMenuItemViewController:[[AccelerateViewController alloc] init] andTitle:@"Accelerate"];
 
     // Shadow
     [self addMenuItemViewController:[[ShadowViewController alloc] init] andTitle:@"Shadow"];
 
-    // UIPasteboard
-    [self addMenuItemViewController:[[UIPasteboardViewController alloc] init] andTitle:@"Copy And Paste"];
-
     // XIBTest
-    UIStoryboard* board = [UIStoryboard storyboardWithName:@"XIBTest" bundle:[NSBundle mainBundle]];
-    UIViewController* controller = [board instantiateInitialViewController];
+    UIStoryboard *board = [UIStoryboard storyboardWithName:@"XIBTest" bundle:[NSBundle mainBundle]];
+    UIViewController *controller = [board instantiateInitialViewController];
 
     [self addMenuItemViewController:controller andTitle:@"XIB Test"];
 }
