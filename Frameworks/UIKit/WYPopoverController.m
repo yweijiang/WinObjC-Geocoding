0af2efb5a1b746f980d2b699326881fbc8f2c4f5
Shahmeer Esmail <shahmeer.esmail@intel.com>
2016-07-07 12:31:05 -0700

    In UIColor::getValueOfRed, default to RGB colorSpaceModel instead of Monochrome when no colorSpaceRef is availlable.

--- Frameworks/UIKit/WYPopoverController.m
+++ Frameworks/UIKit/WYPopoverController.m
@@ -81,8 +81,8 @@
         colorSpaceModel = CGColorSpaceGetModel(colorSpace);
         CGColorSpaceRelease(colorSpace);
     } else {
-        // Default to monochrome in the absence of a colorSpace
-        colorSpaceModel = kCGColorSpaceModelMonochrome;
+        // Default to RGB in the absence of a colorSpace from CGColor
+        colorSpaceModel = kCGColorSpaceModelRGB;
     }
     
     CGFloat rFloat = 0.0, gFloat = 0.0, bFloat = 0.0, aFloat = 0.0;
