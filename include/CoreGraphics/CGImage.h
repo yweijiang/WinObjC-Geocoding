e0a5b76dd30c610bb4faad68527344e31d1f5cb5
Shahmeer Esmail <shahmeer.esmail@intel.com>
2016-07-08 16:52:35 -0700

    Minor formatting change.

--- include/CoreGraphics/CGImage.h
+++ include/CoreGraphics/CGImage.h
@@ -87,4 +87,7 @@ COREGRAPHICS_EXPORT bool CGImageIsMask(CGImageRef image) STUB_METHOD;
 COREGRAPHICS_EXPORT size_t CGImageGetHeight(CGImageRef image);
 COREGRAPHICS_EXPORT size_t CGImageGetWidth(CGImageRef image);
 
-COREGRAPHICS_EXPORT inline surfaceFormat _CGImageGetFormat(unsigned int bitsPerComponent, unsigned int bitsPerPixel, CGColorSpaceRef colorSpace, CGBitmapInfo bitmapInfo);
+COREGRAPHICS_EXPORT inline surfaceFormat _CGImageGetFormat(unsigned int bitsPerComponent,
+                                                           unsigned int bitsPerPixel,
+                                                           CGColorSpaceRef colorSpace,
+                                                           CGBitmapInfo bitmapInfo);
